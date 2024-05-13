53db805502622d227f0ea294fcd9a98e472b11ce
Jjpitchford1 <143430602+Jjpitchford1@users.noreply.github.com>
2024-05-13 15:02:10 +0000

    d

--- script.js
+++ script.js
@@ -1,28 +1,68 @@
 // Declare your arrays here
-let happy = document.querySelector("#happy");
-console.log(happy)
-let happiest = ["Moana.png"];
+let moods = document.querySelector("#moods");
+console.log(moods)
+let happy = ["Moana.png", "Tangled.png"];
+let sad = ["Annie.png", "B&B.png"];
+let spooky = ["Getout.png", "US.png"];
+let nostalgic = ["LM.png", "CampR.png"];
 
-
-happiest.forEach(function(x){
-   console.log(x);
-});
 
 // Make sure to declare your HTML elements as variables! 
 
 // Submit Button 
 let submitButton = document.querySelector("button");
-
-
-
 submitButton.addEventListener("click", function() {
-   
 
 let user = document.querySelector("input").value;
-if (user === "Happy"){
-    let img = document.createElement("img");
-    img.src = happiest;
-   
-}
+if (user === "happy"){
 
+    happy.forEach(function(happy){
+        console.log(happy);
+        let img = document.createElement("img");
+        img.src = happy;
+moods.appendChild(img);
+     });
+}
 });
+submitButton.addEventListener("click", function() {
+   
+    let user = document.querySelector("input").value;
+    if (user === "sad"){
+    
+        sad.forEach(function(sad){
+            console.log(sad);
+            let img = document.createElement("img");
+            img.src = sad;
+    moods.appendChild(img);
+         });
+       
+    }
+    });
+    submitButton.addEventListener("click", function() {
+
+        let user = document.querySelector("input").value;
+        if (user === "spooky"){
+        
+            spooky.forEach(function(spooky){
+                console.log(spooky);
+                let img = document.createElement("img");
+                img.src = spooky;
+        moods.appendChild(img);
+             });
+           
+        }
+        });
+        submitButton.addEventListener("click", function() {
+   
+            let user = document.querySelector("input").value;
+            if (user === "nostalgic"){
+            
+                nostalgic.forEach(function(nostalgic){
+                    console.log(nostalgic);
+                    let img = document.createElement("img");
+                    img.src = nostalgic;
+            moods.appendChild(img);
+                 });
+               
+            }
+            });

4d0bb1fa87a9526ccd31ced9142a2f2587497e62
Jjpitchford1 <143430602+Jjpitchford1@users.noreply.github.com>
2024-05-13 15:02:04 +0000

    x

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 // Declare your arrays here
+let happy = document.querySelector("#happy");
+console.log(happy)
+let happiest = ["Moana.png"];
+
+
+happiest.forEach(function(x){
+   console.log(x);
+});
 
 // Make sure to declare your HTML elements as variables! 
 
 // Submit Button 
 let submitButton = document.querySelector("button");
-submitButton.addEventListener("click", function() {});
+
+
+
+submitButton.addEventListener("click", function() {
+   
+
+let user = document.querySelector("input").value;
+if (user === "Happy"){
+    let img = document.createElement("img");
+    img.src = happiest;
+   
+}
+
+});
